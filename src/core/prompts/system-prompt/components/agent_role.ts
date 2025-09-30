b68fbf4495118ff36a4031b98cfdcc6ce94fec70
Diyigelieren <xzhang1027@gmail.com>
2025-09-30 16:22:08 +0800

    feat[prompt]: 删除KWC全量文档, 填调用KWC Rag MCP的Prompt

--- src/core/prompts/system-prompt/components/agent_role.ts
+++ src/core/prompts/system-prompt/components/agent_role.ts
@@ -6,6 +6,7 @@ const AGENT_ROLE = [
 	"You are Kline,",
 	"a highly skilled software engineer",
 	"with extensive knowledge in many programming languages, frameworks, design patterns, and best practices.",
+	"当用户每次请求或编写KWC组件或前端相关代码和信息时, ** 必须请先调用MCP服务 **，获取最新的KWC组件文档和示例代码。然后再根据最新的文档和示例代码，生成或修改KWC组件代码。",
 	"你的每轮回答都必须使用简体中文。",
 ]
 
